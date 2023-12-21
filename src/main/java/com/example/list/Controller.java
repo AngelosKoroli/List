9ae8a40ce36b32d2eb6b5aa37a3a959c0ef7baab
angeloskoroli <akoroli25@scienceleadership.org>
2023-12-21 09:13:49 -0500

    bruh

--- src/main/java/com/example/list/Controller.java
+++ src/main/java/com/example/list/Controller.java
@@ -1,9 +1,7 @@
 package com.example.list;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 
-public class HelloController {
+public class Controller {
     public TableView AnimeTierlist;
 }
