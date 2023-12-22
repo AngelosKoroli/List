fd062a54cef09fd9f9159a458b354808f1c18302
angeloskoroli <akoroli25@scienceleadership.org>
2023-12-22 09:43:59 -0500

    bruh

---
+++ src/main/java/AnimeTierList.java
@@ -0,0 +1,65 @@
+public class AnimeTierList {
+    private String S;
+    private String A;
+    private String B;
+    private String C;
+    private String D;
+    private String F;
+
+    public AnimeTierList(String s, String a, String b, String c, String d, String f) {
+        S = s;
+        A = a;
+        B = b;
+        C = c;
+        D = d;
+        F = f;
+    }
+
+    public String getS() {
+        return S;
+    }
+
+    public void setS(String s) {
+        S = s;
+    }
+
+    public String getA() {
+        return A;
+    }
+
+    public void setA(String a) {
+        A = a;
+    }
+
+    public String getB() {
+        return B;
+    }
+
+    public void setB(String b) {
+        B = b;
+    }
+
+    public String getC() {
+        return C;
+    }
+
+    public void setC(String c) {
+        C = c;
+    }
+
+    public String getD() {
+        return D;
+    }
+
+    public void setD(String d) {
+        D = d;
+    }
+
+    public String getF() {
+        return F;
+    }
+
+    public void setF(String f) {
+        F = f;
+    }
+}
