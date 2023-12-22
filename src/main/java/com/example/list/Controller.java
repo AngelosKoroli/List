e001bb5df52c9ce4df584e654e18894ddc66d878
jadenwilliams <jaw98x@gmail.com>
2023-12-22 09:51:49 -0500

    kj

--- src/main/java/com/example/list/Controller.java
+++ src/main/java/com/example/list/Controller.java
@@ -1,8 +1,18 @@
 package com.example.list;
 
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 public class Controller {
     public TableView AnimeTierlist;
+   public TableColumn S;
+    public TableColumn A;
+    public TableColumn B;
+    public TableColumn C;
+    public TableColumn D;
+    public TableColumn F;
+
+    public void initialize() {}
+
     //Angelos K was here
 }
